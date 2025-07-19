d4be219e02faee61187fadad578203970d1bbf74
dev <sups56918@gmail.com>
2025-07-19 09:15:49 +0700

    fix: expose port and host for Render

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',                 // Cho phép truy cập từ bên ngoài
+    port: process.env.PORT || 5173  // Dùng PORT do Render cung cấp
+  }
 })
